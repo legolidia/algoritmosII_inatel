603439129923043fc8fe1135e223833a6c6c2142
lídia <lidiacarolinaandrade@gmail.com>
2024-02-21 19:34:15 +0000

    exercicio finalizado

--- atividade_buscaSequencial/buscaSequencial.cpp
+++ atividade_buscaSequencial/buscaSequencial.cpp
@@ -11,20 +11,20 @@ struct Aluno
 };
 
 
-// 1 + 22n pior caso 
+//1 + 19n
 int buscaPorMatricula(int matricula, Aluno alunos[], int tam)
 {
 	// 1 + 6n  
 	for (int i = 0; i < tam; i++)
 	{
-		// 4
+		// 5n		
 		if (alunos[i].matricula == matricula)
 		{
-			// 9 
+			// 9n
 			cout << "Nome: " << alunos[i].nome << " | Matricula: " << alunos[i].matricula << " | Nota: " << alunos[i].nota << endl;
 			break;
 		}
-		// 3
+		// 3n
 		if(i == tam - 1)
 			cout << "Nenhum aluno com essa matricula foi encontrado." << endl;
 	}
@@ -32,27 +32,27 @@ int buscaPorMatricula(int matricula, Aluno alunos[], int tam)
 }
 
 
-//1 + 22n pior caso
+//1 + 19n
 int buscaPorNome(string nome, Aluno alunos[], int tam)
 {
 	//1 + 6n
 	for (int i = 0; i < tam; i++)
 	{
-		//4
+		//5n
 		if (alunos[i].nome == nome)
 		{
-			//9
+			//9n
 			cout << "Nome: " << alunos[i].nome << " | Matricula: " << alunos[i].matricula << " | Nota: " << alunos[i].nota << endl;
 			break;
 		}
-		//3
+		//3n
 		if(i == tam - 1)
 			cout << "Nenhum aluno com este nome foi encontrado." << endl;
 	}
 	return -1;
 }
 
-//2 + 13n
+//2 + 14n
 int buscaPorNota(float nota, Aluno alunos[], int tam)
 {
 	//1
@@ -60,9 +60,9 @@ int buscaPorNota(float nota, Aluno alunos[], int tam)
 	
 	//1 + 6n
 	for (int i = 0; i < tam; i++)
-		//4
+		//5n
 		if (alunos[i].nota <= nota)
-			//3
+			//3n
 			cont++; // cont = cont + 1
 			
 	return cont;
@@ -75,9 +75,9 @@ int main()
 	{
 		{"Taylor", 1, 7.5},
 		{"Swift", 2, 8.0},
-		{"Betty", 3, 6.5},
-		{"James", 4, 9.0},
-		{"Augustine", 5, 5.0}
+		{"Olivia", 3, 6.5},
+		{"Meredith", 4, 9.0},
+		{"Benjamin", 5, 5.0}
 	};
 
 	//0
@@ -96,44 +96,63 @@ int main()
 	cout << endl;
 
 	
-	//
+	//3 + 28n
 	do
 	{
 		cout << "Digite a opcao desejada" << endl;
 		//1
 		cin >> op;
 
+	//11 + 28n (pior caso possível)
 		switch(op)
 		{
+		//1
 		case 1:
 		{
+			//2+19n
 			cout << "Digite a matricula que deseja buscar: " << endl;
+			//1
 			cin >> matricula;
+			//1+19n
 			buscaPorMatricula(matricula, alunos, 5);
 			break;
 
 		}
+		//1
 		case 2:
 		{
+			//2+19n
 			cout << "Digite o nome que deseja buscar: " << endl;
+			//1
 			cin >> nome;
+			//1+19n
 			buscaPorNome(nome, alunos, 5);
 			break;
 
 		}
+		
+		//1
 		case 3:
+		//8 + 28n
 			cout << "Digite a nota para encontrar alunos com a nota inferior" << endl;
+			//1
 			cin >> nota;
+			//3+14n		 
 			if(buscaPorNota(nota, alunos, 5) != 0)
+				//3+14n 																
 				cout << buscaPorNota(nota, alunos, 5) << " alunos tiraram nota abaixo de " << nota << endl;
 			else
+				//1
 				cout << "Nenhum aluno tirou nota abaixo de " << nota << endl;
 			break;
 		}
 	}
+	// 2
 	while(op != 0);
 
 	cin.get();
 
 	return 0;
 }
+
+//PIOR CASO: 3 + 28n -> O(n)
