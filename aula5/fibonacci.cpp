8369a319e5366021ca2c588adb4e97e9345af565
lídia <lidiacarolinaandrade@gmail.com>
2024-03-14 21:47:38 +0000

    aula 5

--- aula5/fibonacci.cpp
+++ aula5/fibonacci.cpp
@@ -3,9 +3,7 @@ using namespace std;
 
 int fibonacci(int n)
 {
-    if (n==0)
-        return 1;
-    if(n==1)
+    if (n==0 || n==1)
         return 1;
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
